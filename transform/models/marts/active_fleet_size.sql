966de2483c03469d87f308ee8844bd6b6fb738cd
Justin Duckett <justin.j.duckett@gmail.com>
2026-03-11 11:28:01 -0400

    delete dbt transform folder

--- transform/models/marts/active_fleet_size.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT
-  snapshot_time,
-  SUM(num_bikes_available) as total_fleet_size
-FROM
-  {{ source('raw_data', 'status_history') }}
-GROUP BY
-  snapshot_time
